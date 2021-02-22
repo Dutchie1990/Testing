9d12710a47503e97e7f31ecc6031728d94089576
Daniel <danielboone1990@gmail.com>
2021-02-22 11:41:59 +0100

    Setting up test for statuscode 500 and testing response body

--- test/API-tests.js
+++ test/API-tests.js
@@ -4,9 +4,21 @@ const chai = require('chai')
 const request = supertest('https://api-pub.bitfinex.com/v2/');
 
 describe('Platform status', () => {
-    it('GET /platform/status - 200', (done) => {
+    it('GET /platform/status - statuscode 200', (done) => {
         request.get('platform/status').end((err, res) => {
             chai.expect(res.statusCode).to.equal(200)
+            done();
+        });
+    });
+    it('GET /platform/status - statuscode 500', (done) => {
+        request.get('platform/stat').end((err, res) => {
+            chai.expect(res.statusCode).to.equal(500)
+            done();
+        });
+    });
+    it('GET /platform/status - response body', (done) => {
+        request.get('platform/status').end((err, res) => {
+            chai.expect(JSON.stringify(res.body)).to.equal('[1]')
             done();
         });
     });
